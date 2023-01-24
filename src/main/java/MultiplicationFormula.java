5fc02e9ccbc4dd03a65d306292c34321554520d6
JuliansRecent <juliansrecent@gmail.com>
2023-01-23 17:19:51 -0800

    Module complete. Prompt the user for two numbers, then print the multiplication of the numbers and product to the console.

--- src/main/java/MultiplicationFormula.java
+++ src/main/java/MultiplicationFormula.java
@@ -7,6 +7,14 @@ public class MultiplicationFormula {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        // Prompt the user for two numbers
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        // Calculate the product of the two numbers
+        int product = firstNumber * secondNumber;
+        // Print the operation and the product to the console
+        System.out.println(firstNumber + " * " + secondNumber + " = " + product);
     }
 }
